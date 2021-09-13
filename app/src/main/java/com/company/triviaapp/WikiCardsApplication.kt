e922e6163eafa2c6d75dfcd9f9f5e99872a7dafa
BunsenBurnsRust <77797048+BunsenBurnsRust@users.noreply.github.com>
2021-09-13 19:02:17 -0400

    Fix to Hilt manifest

---
+++ app/src/main/java/com/company/triviaapp/WikiCardsApplication.kt
@@ -0,0 +1,10 @@
+package com.company.triviaapp
+
+// https://github.com/philipplackner/CryptocurrencyAppYT/blob/app/app/src/main/java/com/plcoding/cryptocurrencyappyt/CoinApplication.kt
+
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WikiCardsApplication : Application()
